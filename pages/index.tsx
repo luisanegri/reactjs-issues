7e9df0b7e2e9ba5e8fc6bc67e2599372f8197e45
luisanegri <luisaanegri@gmail.com>
2022-10-14 14:32:37 +0200

    query issues and add to props

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,22 @@
 import type { NextPage } from 'next';
 import styles from '../styles/Home.module.css';
-import { handler } from './api/service';
+import { queryApi, IIssue } from './api/service';
 
-const Home: NextPage = () => {
+const Home: NextPage<IIssue[]> = ({ issues }) => {
   return (
     <div className={styles.container} onClick={() => handler()}>
       <h1>hello world</h1>
     </div>
   )
+
+export async function getStaticProps() {
+  const issues = await queryApi();
+
+  return {
+    props: {
+      issues,
+    },
+  };
 }
 
-export default Home
+export default Home;
