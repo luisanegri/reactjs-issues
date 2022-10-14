2a8ac4b27465cfa0b397ba6fe52e2d1c0067aa7a
luisanegri <luisaanegri@gmail.com>
2022-10-14 13:46:30 +0200

    add github pat to nextConfig

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  env: {
+    GITHUB_PAT: process.env.GITHUB_PAT,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
